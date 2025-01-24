62a91d9d5a3b3c877feb3683298f0f89d9fdd4db
Rishu148 <dhakedroopkishor@gmail.com>
2025-01-24 10:36:04 +0530

    Add files via upload

---
+++ assignment/23 january.js
@@ -0,0 +1,124 @@
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+
+// }
+
+
+// for(let i=1; i<=20; i++){
+//     if(i % 2!=0){
+
+//         console.log(i);
+
+//     }
+
+// }
+
+
+
+//Medium:
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i * i);
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+
+//         console.log(i);
+//     }
+// }
+
+
+//Difficult:
+
+
+
+// let a=10;
+// b=0;
+// for(let i=0; i<=a; i++){
+//   b+=i;
+//   console.log(b);
+
+
+// }
+
+
+// let a = 0;
+// let b = 1;
+
+// console.log(a); 
+// console.log(b); 
+
+// for (let i = 1; i < 10; i++) {
+//   let c = a + b;
+//   console.log(c);
+//   a = b;
+//   b = c;
+// }
+
+
+
+
+
+
+
+// for(let i=1; i<=5; i++){
+//     str=""
+//     for(let j=1; j<=i; j++){
+//         str+="*"
+//     }
+//     console.log(str);
+
+// }
+
+
+
+
+
+// for(let i=1; i<=10; i++){
+//     console.log("7", "*", i, "=", i*7);
+
+// }
+
+
+// function printCharacters(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         console.log(str[i]);
+//     }
+// }
+
+
+// const myString = "Hello, world!";
+// printCharacters(myString);
+
+
+
+// let a=50;
+// let s = 0;
+// for (let i = 1; i <= a; i++) {
+//     if(i % 2==0){
+//         s += i;
+//     }
+
+// }
+// console.log(s);
+
+
+
+
+
+
+// for (let i = 1; i <= 3; i++) {
+//     let row = " ";
+//     for(let j=3; j>=i; j--){
+//         row+=" "
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         row += j;
+//     }
+//     for (let j = i - 1; j >= 1; j--) {
+//         row += j
+//     }
+//     console.log(row);
+// }
